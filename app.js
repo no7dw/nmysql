7fe02dff690c7c7d5b7f299bcc833ace2feb24af
no7david <no7david@gmail.com>
2015-10-07 21:53:14 +0800

    deploy mysql , but with query error

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ var CRUD = require('./crud');
 
 // First you need to create a connection to the db
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "jay",
-  password: "jay"
+  host: "alij",
+  //port : "3306",
+  user: "root",
+  password: "root"
 });
 
 con.connect(function (err) {
   if (err) {
-    console.log('Error connecting to Db');
+    console.log('Error connecting to Db', err);
     return;
   }
   console.log('Connection established');
--- crud.js
+++ crud.js
@@ -1,11 +1,14 @@
 var async = require('async');
-module.export = {
+module.exports = {
   run: function (con, callback) {
     async.waterfall([
         //query
         function (cb) {
           con.query('SELECT * FROM employees', function (err, rows) {
-            if (err) throw err;
+            if (err) {
+              console.log('err', err);
+              cb(err);
+            }
 
             console.log('Data received from Db:\n');
             console.log(rows);
@@ -16,7 +19,10 @@ module.export = {
         function (rows, cb) {
           var employee = {name: 'Winnie', location: 'Australia'};
           con.query('INSERT INTO employees SET ?', employee, function (err, res) {
-            if (err) throw err;
+            if (err) {
+              console.log('err', err);
+              cb(err);
+            }
 
             console.log('Last insert ID:', res.insertId);
             cb(null, res.insertId);
@@ -28,7 +34,10 @@ module.export = {
             'UPDATE employees SET location = ? Where ID = ?',
             ["South Africa", 5],
             function (err, result) {
-              if (err) throw err;
+              if (err) {
+                console.log('err', err);
+                cb(err);
+              }
 
               console.log('Changed ' + result.changedRows + ' rows', result);
               cb(null, result);
@@ -41,7 +50,10 @@ module.export = {
             'DELETE FROM employees WHERE id = ?',
             [5],
             function (err, updateResult) {
-              if (err) throw err;
+              if (err) {
+                console.log('err', err);
+                cb(err);
+              }
 
               console.log('Deleted ' + updateResult.affectedRows + ' rows');
               cb(null, updateResult);
